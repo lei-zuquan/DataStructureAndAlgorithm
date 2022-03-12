dc18bbdb993f3c5403c101337b1bb3a55e872dfe
lei-zuquan <843291011@qq.com>
2022-03-12 18:45:16 +0800

    # 测试 数组模拟队列

--- src/main/java/com/data_structure/C02_ArrayQueue.java
+++ src/main/java/com/data_structure/C02_ArrayQueue.java
@@ -1,8 +1,59 @@
 package com.data_structure;
 
+
+import java.util.Scanner;
+
 public class C02_ArrayQueue {
     public static void main(String[] args) {
+        // 测试数组模拟队列
+        // 创建一个队列
+        ArrayQueue queue = new ArrayQueue(3);
+        char key = ' '; // 接收用户输入
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        // 输出一个菜单
+        while (loop) {
+            System.out.println("s(show): 显示队列");
+            System.out.println("e(exit): 退出程序");
+            System.out.println("a(add): 添加数据到队列");
+            System.out.println("g(get): 从队列取出数据");
+            System.out.println("h(head): 查看队列头的数据");
+            key = scanner.next().charAt(0); // 接收一个字符
+            switch (key) {
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("输入一个数");
+                    int value = scanner.nextInt();
+                    queue.addQueue(value);
+                    break;
+                case 'g': // 取出数据
+                    try {
+                        int res = queue.getQueue();
+                        System.out.printf("取出的数据是：%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'h':
+                    try {
+                        int res = queue.headQueue();
+                        System.out.printf("队列头的数据是：%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
 
+        }
+        System.out.println("程序退出");
     }
 }
 
@@ -62,7 +113,7 @@ class ArrayQueue {
             return;
         }
         for (int i = 0; i < arr.length; i++) {
-            System.out.printf("arr[%d]=%d\t", arr[i]);
+            System.out.printf("arr[%d]=%d\t", i, arr[i]);
         }
     }
 
