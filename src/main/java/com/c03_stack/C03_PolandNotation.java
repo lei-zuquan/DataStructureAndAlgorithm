995d6b697423c85e1eadeb44d0956defa141d5a7
lei-zuquan <843291011@qq.com>
2022-03-27 19:56:47 +0800

    # 新增 逆波兰表达式实现复杂运算

--- src/main/java/com/c03_stack/C03_PolandNotation.java
+++ src/main/java/com/c03_stack/C03_PolandNotation.java
@@ -8,8 +8,10 @@ public class C03_PolandNotation {
     public static void main(String[] args) {
         // 先定义个逆波兰表达式
         // (3+4)*5 - 6 => 3 4 + 5 * 6 -
+        // 4 * 5 - 8 + 60 + 8 / 2 = 4 5 * 8 - 60 + 8 2 / +
         // 说明为了方便，逆波兰表达式的数字和符号使用空格隔开
-        String suffixExpression = "3 4 + 5 * 6 -";
+        // String suffixExpression = "3 4 + 5 * 6 -";
+        String suffixExpression = "4 5 * 8 - 60 + 8 2 / +"; // 76
         // 思路
         // 1. 先将"3 4 + 5 * 6 -" => 放到ArrayList 中
         // 2. 将ArrayList 传递给一个方法，遍历ArrayList 配合栈完成计算
