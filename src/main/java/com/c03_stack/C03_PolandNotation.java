9bca6b8870908071711ea9d3208c8f0474e8d00e
lei-zuquan <843291011@qq.com>
2022-03-29 08:14:33 +0800

    # 新增 中缀表达式 转 后缀表达式

--- src/main/java/com/c03_stack/C03_PolandNotation.java
+++ src/main/java/com/c03_stack/C03_PolandNotation.java
@@ -11,9 +11,14 @@ public class C03_PolandNotation {
         // 1.   1+((2+3)*4)-5 => 转成 1 2 3 4 * + 5 -
         // 2.   因为直接对str 进行操作，不方便，因此先将“1+((2+3)*4)-5” =》中缀的表达式对应的List
         //      即“1+((2+3)*4)-5”  =》 ArrayList [1,+,(,(,2,+,3,),*,4),-,5]
+        // 3 .  将得到的中缀表达式对应的List ==》 后缀表达式对应的List
+        //      即ArrayList [1,+,(,(,2,+,3,),*,4),-,5] ==>  ArrayList [1,2,3,+,4,*,+,5,-]
         String expression = "1+((2+3)*4)-5";
         List<String> infixExpressionList = toInfixExpressionList(expression);
-        System.out.println(infixExpressionList); // ArrayList [1,+,(,(,2,+,3,),*,4),-,5]
+        System.out.println(infixExpressionList); // ArrayList [1,2,3,+,4,*,+,5,-]
+
+        List<String> suffixExpression = parseSuffixExpression(infixExpressionList);
+        System.out.println("后缀表达式对应的List:" + suffixExpression); // ArrayList [1,2,3,+,4,*,+,5,-]
 
         /*
         // 先定义个逆波兰表达式
@@ -30,6 +35,47 @@ public class C03_PolandNotation {
         int res = calculate(list);
         System.out.println("计算的结果是=" + res);
         */
+    }
+
+    // 即ArrayList [1,+,(,(,2,+,3,),*,4),-,5] ==>  ArrayList [1,2,3,+,4,*,+,5,-]
+    // 方法：将得到的中缀表达式对应的List ==> 后缀表达式对应的List
+    public static List<String> parseSuffixExpression(List<String> ls) {
+        // 定义两个栈
+        Stack<String> s1 = new Stack<>(); // 符号栈
+        // 说明：因为s2 这个栈，在整个转换过程中，没有pop 操作，而且后面我们还需要逆序输出
+        // 因此比较麻烦，这里我们就不用Stack<String> 直接使用List<String> s2
+        // Stack<String> s2 = new Stack<>(); // 存储中间结果的栈s2
+        ArrayList<String> s2 = new ArrayList<>(); // 存储中间结果的List s2
+
+        // 遍历ls
+        for (String item: ls) {
+            // 如果是一个数，加入s2
+            if (item.matches("\\d+")) {
+                s2.add(item);
+            } else if (item.equals("(")) {
+                s1.push(item);
+            } else if (item.equals(")")) {
+                // 如果是右括号“）”，则依次弹出s1 栈顶的运算符，并压入s2，直到遇到左括号为止，此时将这一对括号丢弃
+                while (!s1.peek().equals("(")) {
+                    s2.add(s1.pop());
+                }
+                s1.pop(); // !!! 将（ 弹出s1栈，消除小括号
+            } else {
+                // 当item 的优先级小于等于s1栈顶运算符，将s1栈顶的运算符弹出并加入到s2中，再次转到（4.1）与s1中新的栈顶运算符相比较
+                // 问题 ：我们缺少一个比较优先级高低的方法
+                while (s1.size() != 0 && Operation.getValue(s1.peek()) >= Operation.getValue(item)) {
+                    s2.add(s1.pop());
+                }
+                // 还需要将item 压入栈
+                s1.push(item);
+            }
+        }
+        // 将s1 中剩余的运算符依次弹出并加入s2
+        while (s1.size() != 0) {
+            s2.add(s1.pop());
+        }
+
+        return s2; // 注意因为是存放到List， 因此按顺序输出就是对应的后缀表达式对应的List
     }
 
     // 方法，将中缀表达式转成对应的list
@@ -109,5 +155,38 @@ public class C03_PolandNotation {
         }
         // 最后留在stack 中的数据是运算结果
         return Integer.parseInt(stack.pop());
+    }
+}
+
+
+// 编写一个类Operation 可以返回一个运算符，对应的优先级
+class Operation {
+    private static int ADD = 1;
+    private static int SUB = 1;
+    private static int MUL = 2;
+    private static int DIV = 2;
+
+    // 写一个方法，返回对应的优先级数字
+    public static int getValue(String operation) {
+        int result = 0;
+        switch (operation) {
+            case "+":
+                result = ADD;
+                break;
+            case "-":
+                result = SUB;
+                break;
+           case "*":
+                result = MUL;
+                break;
+            case "/":
+                result = DIV;
+                break;
+            default:
+                //throw new RuntimeException("非法运算符");
+                System.out.println("非法运算符");
+                break;
+        }
+        return result;
     }
 }
